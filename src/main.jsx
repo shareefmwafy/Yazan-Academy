c50cc5a4130460ba10709ada657a1e2bf56f9796
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-02-08 22:52:39 +0200

    Edit Paths

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css';
 import "./i18n";
 import { ThemeProvider } from './cont/ThemeProvider.jsx';
-import { LanguageProvider } from "./cont/LanguageProvider.js";
+import { LanguageProvider } from "./cont/LanguageProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider>
