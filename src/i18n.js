83d461e70dda1a1e043a1cb90cb4e6a0a4293917
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-02-08 22:58:18 +0200

    Edit

--- src/i18n.js
+++ src/i18n.js
@@ -3,6 +3,7 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 import en from "./locales/en.json";
+
 import ar from "./locales/ar.json";
 
 i18n
