ec1de2270d35235b38a279a322561a2cc6a2e80f
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-02-08 21:23:56 +0200

    Create Footer

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeProvider } from './Context/ThemeProvider'
+import { LanguageProvider } from "./context/LanguageProvider";
+
+import "./i18n";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider>
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
+    
+  </ThemeProvider>,
 )
