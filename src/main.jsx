982658db58b2da84d6b9ffbd71896c7a944afc17
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-02-08 21:53:32 +0200

    Edit Paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
-import { ThemeProvider } from './context/ThemeProvider.jsx';
-import { LanguageProvider } from "./context/LanguageProvider.jsx";
+import { ThemeProvider } from './context/ThemeProvider';
+import { LanguageProvider } from "./context/LanguageProvider";
 
 import "./i18n";
 
