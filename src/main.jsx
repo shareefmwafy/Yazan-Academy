1c8161a72d2fcace2dcb254d89fc376d3ae5f022
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-02-08 21:45:02 +0200

    Edit Nav

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ThemeProvider } from './Context/ThemeProvider';
-import { LanguageProvider } from "./context/LanguageProvider";
+import { ThemeProvider } from '/Context/ThemeProvider';
+import { LanguageProvider } from "/context/LanguageProvider";
 
 import "./i18n";
 
