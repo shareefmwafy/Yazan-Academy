f76b04f26f5e3d5bf82790be6bd63833689ff684
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-02-08 22:18:44 +0200

    Edit Vite Config

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
 // import { ThemeProvider } from './context/ThemeProvider.js';
-import { LanguageProvider } from "./context/LanguageProvider";
+import { LanguageProvider } from "@/Context/LanguageProvider";
 import { ThemeProvider } from './context/ThemeProvider';
 import "./i18n";
 
