67a18b0506a8f374b4a21bd04e634ebfdd4472cf
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-02-08 22:41:55 +0200

    Edit File Name

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
 // import { ThemeProvider } from './context/ThemeProvider.js';
-import { LanguageProvider } from "@/context/LanguageProvider";
-import { ThemeProvider } from './context/ThemeProvider';
+import { LanguageProvider } from "./context/LanguageProvider.jsx";
+import { ThemeProvider } from './context/ThemeProvider.jsx';
 import "./i18n";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
