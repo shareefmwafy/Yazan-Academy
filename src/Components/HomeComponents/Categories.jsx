f6db3551dc06e56d564d3fe3557ddeb69770dd0e
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-02-09 23:59:59 +0200

    Add Medical terminology class Section

--- src/Components/HomeComponents/Categories.jsx
+++ src/Components/HomeComponents/Categories.jsx
@@ -4,6 +4,7 @@ import { useTheme } from '../../cont/ThemeProvider';
 
 import English from '../../assets/English.png';
 import Psychology from '../../assets/Psychology.png';
+import MedicalTerminologyImage from '../../assets/MedicalTerminology.png';
 
 function Categories() {
     const { language } = useLanguage();
@@ -14,32 +15,37 @@ function Categories() {
             title: "Choose Appointment Type",
             englishClass: "Book English Sessions",
             therapyClass: "Book Therapy ",
+            MedicalTerminologyClass: "Medical Terminology Class",
         },
         ar: {
             title: "اختر نوع الحجز",
             englishClass: "حجز موعد لحصة اللغة الإنجليزية",
             therapyClass: "حجز موعد للعلاج النفسي",
+            MedicalTerminologyClass: "حصة مصطلحات طبية"
         }
     };
 
     const currentText = language === 'ar' ? texts.ar : texts.en;
 
     return (
-        <div className={`mt-10 mb-10 w-[95%] mx-auto p-6 rounded-2xl relative overflow-hidden`}
+        <div className={`mt-10 mb-10 w-[95%] mx-auto p-6 rounded-2xl relative overflow-hidden `}
             style={{
                 backgroundColor: 'var(--cat-bg-color)',
                 color: 'var(--cat-color)',
-
             }}
         >
             <div className="absolute inset-0 bg-gradient-to-br from-primary to-secondary opacity-10"></div>
             <h2 className="text-3xl sm:text-4xl md:text-5xl font-extrabold text-center mb-8 relative z-10 tracking-wide drop-shadow-lg break-words px-4">
                 {currentText.title}
             </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 relative z-10">
+            <div className="flex gap-8 relative z-10 flex-wrap justify-between">
                 {/* English Class Booking Card */}
-                <div className="flex justify-center">
-                    <div className="w-fit sm:w-96  p-8 rounded-xl transition duration-300 ease-in-out  overflow-hidden border-2 border-white">
+                <div className="flex justify-center ">
+                    <div className="w-fit sm:w-96 p-8 rounded-xl transition duration-300 ease-in-out overflow-hidden shadow-lg"
+                        style={{
+                            boxShadow: "0 4px 6px var(--shadow-color)",
+                        }}
+                    >
                         <img src={English} alt="English Class" className="w-full h-48 object-contain rounded-t-xl" />
                         <div className="p-6 text-center">
                             <button
@@ -52,14 +58,46 @@ function Categories() {
                     </div>
                 </div>
 
+                {/* Medical Terminology Class Card */}
+                <div className="flex justify-center "
+                    style={{
+                        borderColor: 'var(--cat-border)',
+                    }}
+                >
+                    <div className="w-fit sm:w-96 p-8 rounded-xl transition duration-300 ease-in-out overflow-hidden"
+                        style={{
+                            boxShadow: "0 4px 6px var(--shadow-color)",
+                        }}
+                    >
+                        <img src={MedicalTerminologyImage} alt="Medical Terminology Class" className="w-full h-48 object-contain rounded-t-xl" />
+
+                        <div className="p-6 text-center">
+                            <button
+                                onClick={() => alert(currentText.medicalTerminologyClass)}
+                                className="w-full p-2 text-white font-bold text-xl rounded-lg bg-gradient-to-r from-green-400 to-green-600 hover:from-green-500 hover:to-green-700 transition-transform transform shadow-md"
+                            >
+                                {currentText.MedicalTerminologyClass}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
                 {/* Therapy Appointment Booking Card */}
-                <div className="flex justify-center">
-                    <div className="w-full sm:w-96  p-8 rounded-xl transition duration-300 ease-in-out transform overflow-hidden border-2 border-white">
+                <div className="flex justify-center "
+                    style={{
+                        borderColor: 'var(--cat-border)',
+                    }}
+                >
+                    <div className="w-fit sm:w-96 p-8 rounded-xl transition duration-300 ease-in-out overflow-hidden"
+                        style={{
+                            boxShadow: "0 4px 6px var(--shadow-color)",
+                        }}
+                    >
                         <img src={Psychology} alt="Therapy Session" className="w-full h-48 object-contain rounded-t-xl" />
                         <div className="p-6 text-center">
                             <button
                                 onClick={() => alert(currentText.therapyClass)}
-                                className="w-full p-2 text-white font-bold text-xl rounded-lg bg-gradient-to-r from-blue-400 to-blue-600 hover:from-blue-500 hover:to-blue-700 transition-transform transform shadow-md"
+                                className="w-full p-2 text-white font-bold text-xl rounded-lg bg-gradient-to-r from-green-400 to-green-600 hover:from-green-500 hover:to-green-700 transition-transform transform shadow-md"
                             >
                                 {currentText.therapyClass}
                             </button>
@@ -67,6 +105,7 @@ function Categories() {
                     </div>
                 </div>
             </div>
+
         </div>
     );
 }
