03babbaf8f4b5f54b69caaa259d3ca957de0a7ee
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-02-10 22:29:55 +0200

    Add Information Section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../Components/HomeComponents/Hero'
 import Categories from '../Components/HomeComponents/Categories'
+import Info from '../Components/HomeComponents/Info'
 function Home() {
   return (
     <>
       <Hero/>
       <Categories/>
+      <Info/>
     </>
   )
 }
